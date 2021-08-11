e1c9b4414b6e41e0ac02476e07662aadd0bd3b8e
nachwon <nachwon@gmail.com>
2021-08-12 00:37:08 +0900

    Add animation to download button

--- src/components/mixerActions/styles.ts
+++ src/components/mixerActions/styles.ts
@@ -1,6 +1,7 @@
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import Download from "../../assets/download.png";
 import soundhubIcon from "../../assets/soundhub-icon.png";
+import { THEME } from "../../constants";
 
 export const MixerActionButtonsContainer = styled.div`
   width: 100%;
@@ -32,6 +33,36 @@ export const MixerTitle = styled.div`
   color: white;
 `;
 
+const buttonEnabled = keyframes`
+10% {
+  transform: scale(1.5);
+}
+
+50% {
+  transform: scale(1.5) rotate(15deg);
+}
+
+60% {
+  transform: scale(1.5) rotate(-15deg);
+}
+
+70% {
+  transform: scale(1.5) rotate(15deg);
+}
+
+80% {
+  transform: scale(1.5) rotate(-15deg);
+}
+
+85% {
+  transform: scale(1.5) rotate(0deg);
+}
+
+100% {
+  transform: scale(1)
+}
+`;
+
 export const DownloadButton = styled.button`
   background-image: url(${Download});
   background-color: transparent;
@@ -39,13 +70,20 @@ export const DownloadButton = styled.button`
   background-size: 15px;
   height: 25px;
   width: 25px;
-  border: 1px solid #393939;
+  border: 1px solid ${THEME.MAIN_COLOR_BLUE};
   border-radius: 4px;
   background-repeat: no-repeat;
   box-shadow: 1px 1px 2px 0px;
   filter: ${(props: { isLoaded: boolean }) => (props.isLoaded ? "grayscale(0)" : "grayscale(1)")};
-  transition-duration: 0.5s;
+  animation: ${(props: { isLoaded: boolean }) =>
+    props.isLoaded
+      ? css`
+          ${buttonEnabled} linear 1s
+        `
+      : ""};
+
   :active {
     box-shadow: 1px 1px 0px 0px;
+    transform: translateY(1px);
   }
 `;
