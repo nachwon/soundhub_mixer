52baa3b18ac7c99c8fc884700168ec8de7a3b145
nachwon <nachwon@gmail.com>
2021-08-22 00:33:27 +0900

    Add animation to waveform

--- src/components/progressController/waveform/index.tsx
+++ src/components/progressController/waveform/index.tsx
@@ -11,6 +11,7 @@ interface WaveformProps {
 const Waveform: React.FC<WaveformProps> = (props) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [canvasCtx, setCanvasCtx] = useState<CanvasRenderingContext2D>();
+  const frame = useRef(0);
 
   const initCanvas = (canvas: HTMLCanvasElement | null): CanvasRenderingContext2D | undefined => {
     if (canvas === null) {
@@ -37,14 +38,29 @@ const Waveform: React.FC<WaveformProps> = (props) => {
     if (!canvasCtx) {
       return;
     }
-    canvasCtx.clearRect(0, 0, props.width, props.height);
-    props.data.forEach((value, index) => {
-      canvasCtx.fillStyle = "#888888";
-      canvasCtx.fillRect(index * 2, (props.height - value) / 2, 1, value / 2);
 
-      canvasCtx.fillStyle = "#666666";
-      canvasCtx.fillRect(index * 2, props.height / 2, 1, value / 2);
-    });
+    const drawWaveform = (frame: number) => {
+      if (frame === 40) {
+        return;
+      }
+
+      canvasCtx.clearRect(0, 0, props.width, props.height);
+      props.data.forEach((value, index) => {
+        canvasCtx.fillStyle = "#888888";
+        canvasCtx.fillRect(
+          index * 2,
+          Math.max((props.height - value) / 2, (props.height - frame * 2) / 2),
+          1,
+          Math.min(frame, value / 2)
+        );
+
+        canvasCtx.fillStyle = "#666666";
+        canvasCtx.fillRect(index * 2, props.height / 2, 1, Math.min(frame, value / 2));
+      });
+      requestAnimationFrame(() => drawWaveform(frame + 1));
+    };
+
+    requestAnimationFrame(() => drawWaveform(frame.current));
   }, [canvasCtx, props.height, props.width, props.data]);
 
   return <S.WaveformCanvas ref={canvasRef} width={props.width} height={props.height} />;
